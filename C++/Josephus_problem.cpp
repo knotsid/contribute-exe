fb1e6ef394bef3b7913bcc5c109871153659a725
Pallavi Singh <91817009+Pallavisingh123@users.noreply.github.com>
2022-10-04 00:44:00 +0530

    added josephus problem code

--- C++/Josephus_problem.cpp
+++ C++/Josephus_problem.cpp
@@ -1,5 +1,4 @@
-// Josephus problem | (A O(n) Solution)
-// Recursive function to implement the Josephus problem
+
 // Josephus problem | (A O(n) Solution)
 // Recursive function to implement the Josephus problem
 
@@ -24,21 +23,3 @@ int main()
     return 0;
 }
 
-int josephus(int n, int k)
-{
-    if (n == 1)
-        return 1;
-    else
-        // The position returned by josephus(n - 1, k)
-        // is adjusted because the recursive call
-        // josephus(n - 1, k) considers the
-        // original position k % n + 1 as position 1
-        return (josephus(n - 1, k) + k - 1) % n + 1;
-}
-int main()
-{
-    int n = 14;
-    int k = 2;
-    cout << "The chosen place is " << josephus(n, k);
-    return 0;
-}
