67593317e7c6764308fe8ea31fa1ebfd413242eb
Utkarsh Katiyar <98865912+utkarshkatiyar1@users.noreply.github.com>
2022-10-03 09:55:33 +0530

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;  
+
+class BinarySearchExample2{  
+
+    public static void main(String args[]){  
+
+        int arr[] = {10,20,30,40,50};  
+
+        int key = 30;  
+
+        int result = Arrays.binarySearch(arr,key);  
+
+        if (result < 0)  
+
+            System.out.println("Element is not found!");  
+
+        else  
+
+            System.out.println("Element is found at index: "+result);  
+
+    }  
+
+}  
