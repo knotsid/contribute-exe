d930d6ddf3d8b2257f821c1a89d2bbe0fd2f193f
discodevl <donato.cutro3@gmail.com>
2022-10-04 19:23:03 +0200

    javascript

--- C++/dfs.cpp
+++ C++/dfs.cpp
@@ -1,57 +1,45 @@
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
-void printvector(vector<int>Arr){
-  for(int i=0;i<Arr.size();i++)
-  {
-    cout<<Arr[i]<<" ";
+void print(int **edges,int n,int si,bool *visited){
+    cout<<si<<" ";
+    visited[si]=true;
+    for(int i=0;i<n;i++){
+        if(i==si){
+            continue;
+        }
+        if(edges[si][i]==1){
+            if(visited[i]){
+                continue;
+            }
+            print(edges,n,i,visited);
+        }
+    }
+}
+int main() {
+  int n,e;
+  cin>>n>>e;
+  int **edges=new int*[n];
+  for(int i=0;i<n;i++){
+    edges[i]=new int[n];
+    for(int j=0;j<n;j++){
+        edges[i][j]=0;
+    }
   }
-    cout<<endl;
-}
-void dfs(int node,vector<int>vec[],vector<int>&vis,vector<int>&dfsvec)
-{
-vis[node]=1;
-dfsvec.push_back(node);
-for(auto it:vec[node])
-{ if(!vis[it])
-     {
-       dfs(it,vec,vis,dfsvec)  ;
-
-     }
-}
-}
-vector<int>GraphofDfs(int n,vector<int>vec[])
-{
-
-    vector<int>vis(n+1,0);
-    vector<int>dfsvec;
-    for(int i=1;i<=n;i++)
-    { if(!vis[i]){
-    dfs(i,vec,vis,dfsvec);
+    for(int i=0;i<e;i++){
+        int f,s;
+        cin>>f>>s;
+        edges[f][s]=1;
+        edges[s][f]=1;
     }
+    bool *visited=new bool[n];
+    for(int i=0;i<n;i++){
+        visited[i]=false;
     }
-    return dfsvec;
-}
-
-int main()
-{
-    int N,M;
-    cin>>N>>M;
-    vector<int>V[N+1];
-    for(int i=0;i<M;i++){
-            int v,u;
-           cin>>v>>u;
-        V[v].push_back(u);
-        V[u].push_back(v);
-          }
-for(int i=1;i<=N;i++)
- {
-     cout<<i<<"-";
-     printvector(V[i]);
- }
- vector<int> ans;
- ans=GraphofDfs(N,V);
-printvector(ans);
-
+    print(edges,n,0,visited);
+    for(int i=0;i<n;i++){
+        delete []edges[i];
+    }
+    delete []edges;
+    delete [] visited;
     return 0;
 }
